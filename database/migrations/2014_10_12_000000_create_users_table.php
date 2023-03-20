903b602950f6f4ab1613eb2642c398acd03db8f6
minosys-jp <minoru@minosys.com>
2023-03-20 13:25:04 +0900

    tenant_id for super-user is null

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('tenant_id')->index();
+            $table->unsignedBigInteger('tenant_id')->nullable(true)->index();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
